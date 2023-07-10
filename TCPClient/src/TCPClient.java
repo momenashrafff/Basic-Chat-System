bb3f0d8d8f08465b8fdffa31a3b9cf52cdebfb9b
momenashrafff <momenashraf742@gmail.com>
2023-07-10 12:37:48 +0300

    Initial commit

---
+++ TCPClient/src/TCPClient.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCPClient {
+    public static void main(String[] args) throws Exception {
+        final String[] sentence = new String[1];
+        final String[] modifiedSentence = new String[1];
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("172.20.10.3", 5444);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        sentence[0] = inFromUser.readLine();
+        outToServer.writeBytes(sentence[0] + '\n');
+        outToServer.flush();
+
+        modifiedSentence[0] = inFromServer.readLine();
+
+        if (modifiedSentence[0].equalsIgnoreCase("CONNECT")) {
+            System.out.println("FROM SERVER: The connection is established.");
+            Thread sendThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        sentence[0] = inFromUser.readLine();
+                        outToServer.writeBytes(sentence[0] + '\n');
+                        outToServer.flush();
+
+                        if (sentence[0].equalsIgnoreCase("quit")) {
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        modifiedSentence[0] = inFromServer.readLine();
+                        System.out.println("FROM SERVER: " + modifiedSentence[0]);
+
+                        if (modifiedSentence[0].equalsIgnoreCase("quit")) {
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            sendThread.start();
+            receiveThread.start();
+            sendThread.join();
+            receiveThread.join();
+        }
+
+        clientSocket.close();
+    }
+}
---
+++ TCPServer/src/TCPServer.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+    public static void main(String[] args) throws Exception {
+        String clientSentence;
+        boolean flag = true;
+
+        ServerSocket welcomeSocket = new ServerSocket(5444);
+
+        while (flag) {
+            Socket connectionSocket = welcomeSocket.accept();
+
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientSentence = inFromClient.readLine();
+            if (clientSentence != null && clientSentence.equalsIgnoreCase("connect")) {
+                String capitalizeSentence = clientSentence.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizeSentence);
+                outToClient.flush();
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(System.in));
+
+                while (true) {
+                    if (inFromClient.ready()) {
+                        clientSentence = inFromClient.readLine();
+                        if (clientSentence.equalsIgnoreCase("quit")) {
+                            break;
+                        }
+                        System.out.println("FROM CLIENT: " + clientSentence);
+                    }
+
+                    if (inFromServer.ready()) {
+                        String serverSentence = inFromServer.readLine();
+                        if (serverSentence.equalsIgnoreCase("quit")) {
+                            outToClient.writeBytes(serverSentence + '\n');
+                            outToClient.flush();
+                            break;
+                        }
+                        outToClient.writeBytes(serverSentence + '\n');
+                        outToClient.flush();
+                    }
+                }
+
+                connectionSocket.close();
+            }
+        }
+        welcomeSocket.close();
+    }
+}
